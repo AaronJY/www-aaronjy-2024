299cef20ef3cc9e97fe32b3dfa1faf71bb821b8a
Aaron Yarborough <aaron@alienjungle.digital>
2024-03-13 12:47:16 +0000

    feat: add sitemap

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,8 +1,15 @@
+import { NextSeo } from 'next-seo'
 import React from 'react'
 
 function Article ({ attributes, html }) {
   return (
     <section>
+      <NextSeo title={attributes.title} description={attributes.desc} openGraph={
+        {
+          title: attributes.title,
+          description: attributes.desc
+        }
+      } />
       <article>
         <h1>{attributes.title}</h1>
         <p>{attributes.desc}</p>
