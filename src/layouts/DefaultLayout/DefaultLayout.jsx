cfedcc54a5fa4f47953e47e05540ee1dcf8dc99e
Aaron Yarborough <aaron@alienjungle.digital>
2024-08-23 13:30:43 +0100

    feat: remove ubuntu font

--- src/layouts/DefaultLayout/DefaultLayout.jsx
+++ src/layouts/DefaultLayout/DefaultLayout.jsx
@@ -4,13 +4,9 @@ import style from './DefaultLayout.module.css'
 import Header from '@/components/Header/Header'
 import Footer from '@/components/Footer/Footer'
 
-import { Ubuntu } from 'next/font/google'
-
-const fontMain = Ubuntu({ subsets: ['latin'], weight: ['400', '500'] })
-
 function DefaultLayout ({ children }) {
   return (
-    <main className={`${style.layout} ${fontMain.className}`}>
+    <main className={`${style.layout}`}>
       <Header />
       <article>{children}</article>
       <Footer />
