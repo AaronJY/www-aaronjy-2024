c070e15de0e3461b29211cf43d4ddd0af4428e9c
Aaron Yarborough <aaron@alienjungle.digital>
2025-01-27 18:16:42 +0000

    chore: update layout

--- src/layouts/DefaultLayout/DefaultLayout.jsx
+++ src/layouts/DefaultLayout/DefaultLayout.jsx
@@ -5,13 +5,13 @@ import Header from '@/components/Header/Header'
 import Footer from '@/components/Footer/Footer'
 import Head from 'next/head'
 
-function DefaultLayout({ children }) {
+function DefaultLayout ({ children }) {
   return (
     <main className={`${style.layout}`}>
       <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-<link href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+        <link href='https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,100..900;1,100..900&display=swap' rel='stylesheet' />
       </Head>
       <Header />
       <article>{children}</article>
